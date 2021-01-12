51014c97932b2a1a07ec92dae3ac6068e348baf7
RaymondLoranger <Raymond.Loranger@bell.net>
2021-01-12 15:01:27 -0500

    0.1.14

--- lib/islands/client/player.ex
+++ lib/islands/client/player.ex
@@ -2,13 +2,10 @@
 # │ Inspired by the course "Elixir for Programmers" by Dave Thomas. │
 # └─────────────────────────────────────────────────────────────────┘
 defmodule Islands.Client.Player do
-  use PersistConfig
-
-  @course_ref Application.get_env(@app, :course_ref)
-
   @moduledoc """
   Models a `player` in the _Game of Islands_.
-  \n##### #{@course_ref}
+
+  ##### Inspired by the course [Elixir for Programmers](https://codestool.coding-gnome.com/courses/elixir-for-programmers) by Dave Thomas.
   """
 
   alias IO.ANSI.Plus, as: ANSI
@@ -18,15 +15,14 @@ defmodule Islands.Client.Player do
   # :initialized, :players_set, :player1_turn, :player2_turn, :game_over
   @spec play(State.t()) :: no_return
   def play(%State{tally: %Tally{game_state: game_state}} = state),
-    do: state |> React.react_to(game_state) |> continue()
+    do: React.react_to(state, game_state) |> continue()
 
   ## Private functions
 
   @spec continue(State.t()) :: no_return
   defp continue(%State{tally: %Tally{game_state: :game_over}} = state) do
-    state |> Summary.display() |> GameOver.message() |> ANSI.puts()
-    # GameOver.clear_messages()
-    :lib.flush_receive()
+    Summary.display(state) |> GameOver.message() |> ANSI.puts()
+    GameOver.clear_messages()
     self() |> Process.exit(:normal)
   end
 
